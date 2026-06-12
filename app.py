75d67df7706b75fba8ba7510459e2a53d57f29a1
mohhgh766 <moh.h.gh766@gmail.com>
2026-06-13 00:23:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import streamlit as st
+import yfinance as yf
 
 st.set_page_config(page_title="Abu Hamza Stock Analyzer")
 
 st.title("📊 Abu Hamza Stock Analyzer")
 
-company = st.text_input("اسم الشركة أو الرمز")
+symbol = st.text_input("أدخل رمز السهم")
 
-if company:
-    st.success(f"تم إدخال الشركة: {company}")
+if symbol:
 
-    st.metric("التقييم", "75/100")
+    try:
+        stock = yf.Ticker(symbol)
 
-    st.write("هذه نسخة تجريبية أولية للمشروع.")
+        info = stock.info
+
+        st.subheader(info.get("longName", symbol))
+
+        st.write("### معلومات أساسية")
+
+        st.write(f"القطاع: {info.get('sector', 'غير متوفر')}")
+        st.write(f"الصناعة: {info.get('industry', 'غير متوفر')}")
+        st.write(f"القيمة السوقية: {info.get('marketCap', 'غير متوفر')}")
+        st.write(f"مكرر الربحية: {info.get('trailingPE', 'غير متوفر')}")
+        st.write(f"العائد على حقوق المساهمين ROE: {info.get('returnOnEquity', 'غير متوفر')}")
+
+    except Exception as e:
+        st.error("تعذر جلب البيانات")
